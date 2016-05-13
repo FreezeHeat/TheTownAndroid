324b182bbf9d5beb96251b16b725ad28285dfd81
Asaf.Y <asafyeshayahu@gmail.com>
2016-05-13 19:01:26 +0300

    Game class, NewGame.java

--- app/src/main/java/ben_and_asaf_ttp/thetownproject/NewGame.java
+++ app/src/main/java/ben_and_asaf_ttp/thetownproject/NewGame.java
@@ -2,44 +2,39 @@ package ben_and_asaf_ttp.thetownproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import ben_and_asaf_ttp.thetownproject.DB.DBHandler;
 
 public class NewGame extends AppCompatActivity {
     DBHandler dbHandler;
+    EditText gameDescription;
+    EditText gameNumPlayers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
         dbHandler = new DBHandler(this);
+        gameDescription = (EditText)findViewById(R.id.editGameDescription);
+        gameNumPlayers = (EditText)findViewById((R.id.editNumPlayers));
     }
 
     public void addBook(View v)
     {
-        // get the 3 values from the screen
-        String bookName  = gameDescription.getText().toString();
-        String authorName = .getText().toString();
-        int year = Integer.parseInt(yearEdit.getText().toString());
+        Game game = new Game();
+        game.setDescription(gameDescription.getText().toString());
+        game.setNumPlayers(Integer.parseInt(gameNumPlayers.getText().toString()));
 
-        Book b = new Book(bookName, authorName, year);
-
-        if(dbHandler.addBook(b))
-            Toast.makeText(this, "Book added succesfuly", Toast.LENGTH_SHORT).show();
+        if(dbHandler.addGame(game))
+            Toast.makeText(this, "Game was added succesfuly", Toast.LENGTH_SHORT).show();
         else
-            Toast.makeText(this, "Book NOT added", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Game wasn't added", Toast.LENGTH_SHORT).show();
 
-    }
-
-    public void viewBooks(View v)
-    {
-        ArrayList<Book> booksList = dbHandler.getAllBooks();
-
-        // each round in the loop is a record in the DB
-        for(Book b: booksList)  {
-
-            Log.d("TESTDB", b.toString());
-        }
     }
 }
