6dc09519929887507d744be6b46509de57eb475a
Asaf.Y <asafyeshayahu@gmail.com>
2016-05-13 13:16:03 +0300

    Initial Commit

---
+++ app/src/main/java/ben_and_asaf_ttp/thetownproject/MainActivity.java
@@ -0,0 +1,13 @@
+package ben_and_asaf_ttp.thetownproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
