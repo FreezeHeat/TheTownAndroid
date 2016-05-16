322ee5dc158b88f78fab8c882f2a21ac1bbbff40
Asaf.Y <asafyeshayahu@gmail.com>
2016-05-16 21:53:16 +0300

    Final Edit

--- app/src/main/java/ben_and_asaf_ttp/thetownproject/Login.java
+++ app/src/main/java/ben_and_asaf_ttp/thetownproject/Login.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
     private TextView welcome;
     AlertDialog.Builder builder;
+    static final int MENU_EXIT = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,29 @@ public class Login extends AppCompatActivity {
         welcome.setText(getResources().getString(R.string.txtWelcome) + " "
                 + MainActivity.myPrefs.getString("username", ""));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.exitApp:
+                onBackPressed();
+                break;
+        }
+        return true;
+    }
+
+
 
     public void buildExitDialog(){
         builder = new AlertDialog.Builder(this);
