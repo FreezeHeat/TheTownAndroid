d297efbba90843e78e45747a2830fc284c06d22e
bengilad <bengilad1990@gmail.com>
2016-09-19 12:41:50 +0300

    SettingsActivity

--- app/src/main/java/ben_and_asaf_ttp/thetownproject/SettingsActivity.java
+++ app/src/main/java/ben_and_asaf_ttp/thetownproject/SettingsActivity.java
@@ -1,13 +1,15 @@
 package ben_and_asaf_ttp.thetownproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
+import android.view.View;
 
 /**
  * Created by user on 19/09/2016.
  */
-public class SettingsActivity extends PreferenceActivity{
+public class SettingsActivity extends PreferenceActivity implements View.OnClickListener{
     private CheckBoxPreference checkbox;
 
 
@@ -19,5 +21,16 @@ public class SettingsActivity extends PreferenceActivity{
 
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.pref_btnBack:
+
+                break;
+            default:
+                break;
+        }
     }
 }
